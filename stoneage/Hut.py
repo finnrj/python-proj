69cdc10b0f87c90429658bc74b6876961d4cc838
finnrj <finnrj@gmail.com>
2013-01-24 21:37:22 +0100

    implemented CountHut.missing( ) method

--- stoneage/Hut.py
+++ stoneage/Hut.py
@@ -22,7 +22,11 @@ class Hut:
     def value(self):
         return sum(self._costs)
     
+    def containsOnlyFood(self, resources):
+        return sum([resources.count(num) for num in [3,4,5,6]]) == 0
+    
 class SimpleHut(Hut):
+    """ hut with exactly three resources """
     def __init__(self, r1, r2, r3):
         Hut.__init__(self)
         self._costs = [r1, r2, r3]
@@ -41,6 +45,7 @@ class SimpleHut(Hut):
         return missing
     
 class AnyHut(Hut):
+    """ 1-7 Hut """
     def __init__(self):
         Hut.__init__(self)
         self._costs = []
@@ -51,17 +56,56 @@ class AnyHut(Hut):
         return nonFood[:7]
         
     def missing(self, resources):
-        if len(resources) == 0 or self.onlyFood(resources) :
+        if len(resources) == 0 or self.containsOnlyFood(resources) :
             return [3]
         return [] 
 
-    def onlyFood(self, resources):
-        return sum([resources.count(num) for num in [3,4,5,6]]) == 0
     
 class CountHut(Hut):
-    def __init__(self):
+    """ Hut with four or five resources """
+    def __init__(self, resourceCount, typesCount):
         Hut.__init__(self)
+        self.resourceCount = resourceCount
+        self.typesCount = typesCount
+        self._costs = []
 
+    def costs(self, resources):
+        nonFood = [resource for resource in resources if resource != 2]
+        self._costs = nonFood[:7]
+        return nonFood[:7]
+
+    def tooFewDifferentTypes(self, typesCounts):
+        return typesCounts.count(0) > 4 - self.typesCount
+
+
+    def tooFewResources(self, typesCounts):
+        return sum(typesCounts) < self.resourceCount
+
+    def missing(self, resources):
+        if len(resources) == 0 or self.containsOnlyFood(resources) :
+            necessaryTypes = [3,4,5,6][:self.typesCount] 
+            result = necessaryTypes + (self.resourceCount - self.typesCount) * [3]
+            return result
+        
+        typesCounts = [resources.count(num) for num in [3,4,5,6]]
+        if self.tooFewDifferentTypes(typesCounts):
+            missingTypesCount = typesCounts.count(0) - (4 - self.typesCount)
+            result = []
+            for idx, count in enumerate(typesCounts):
+                if count == 0:
+                    result.append(idx + 3)
+                    missingTypesCount -= 1
+                if missingTypesCount == 0:
+                    break
+            return result + (self.resourceCount - (sum(typesCounts) + len(result))) * [3]
+            
+        if self.tooFewResources(typesCounts):
+            for idx, count in enumerate(typesCounts):
+                if count != 0:   
+                    return (self.resourceCount - sum(typesCounts)) * [idx + 3]
+        return []
+        
+         
 
 def main():
     pass
--- stoneage/HutTest.py
+++ stoneage/HutTest.py
@@ -53,7 +53,49 @@ class HutTest(unittest.TestCase):
         self.assertEqual([3], hut.missing(resources))
         self.assertEqual([], hut.costs(resources))
         
+    def testCountHutWithNoResources(self):
+        hut = CountHut(4,1)
+        resources = []
         
+        self.assertEqual([3,3,3,3], hut.missing(resources))
+        
+        hut = CountHut(4,3)
+        resources = []
+        self.assertListEqual([3,3,4,5], sorted(hut.missing(resources)))
+        
+        hut = CountHut(5,4)
+        resources = []
+        self.assertListEqual([3,3,4,5,6], sorted(hut.missing(resources)))
+
+    def testCountHutWithEnoughResources(self):
+        hut = CountHut(4,2)
+        resources = [2,2,3,3,5,5,5]
+        
+        self.assertEqual([], hut.missing(resources))
+        
+        hut = CountHut(5,3)
+        resources = [2,2,3,3,5,5,5,6]
+        
+        self.assertEqual([], hut.missing(resources))
+
+        
+        
+    def testCountHutWithToFewResources(self):
+        hut = CountHut(4,2)
+        resources = [2,2,3,3,5]
+        
+        self.assertEqual([3], hut.missing(resources))
+        
+        hut = CountHut(4,4)
+        resources = [2,2,3,3,5]
+        
+        self.assertEqual([4,6], hut.missing(resources))
+
+
+        hut = CountHut(5,2)
+        resources = [4,4,4,5]
+        
+        self.assertEqual([4], hut.missing(resources))
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(HutTest)
