fbf26315c46175c5d9deebf81c4ef70b1728e73e
finnrj <finnrj@gmail.com>
2014-11-06 20:48:38 +0100

    possible to win with four connected in row or column

--- gamebots/connect-four/Board.py
+++ gamebots/connect-four/Board.py
@@ -6,8 +6,8 @@ Created on Oct 30, 2014
 
 class Board:
 
-    def __init__(self, rows, cols):
-        self.board= [[None for row in range(rows)] for col in range(cols)]
+    def __init__(self, y, x):
+        self.board= [[None for row in range(y)] for col in range(x)]
         self.move_count=0
     
     def colCount(self):
@@ -19,17 +19,47 @@ class Board:
     def isEmpty(self):
         return self.move_count == 0
     
-    def play(self, column):
-        idx = self.board[column].index(None)
-        self.board[column][idx] = self.move_count % 2
+    def play(self, x):
+        if self.is_col_full(x):
+            return
+        self.board[x][self.board[x].index(None)] = self.getPlayer()
         self.move_count += 1
+        
+    def is_col_full(self, x):
+        return not None in self.board[x]
     
-    def getColor(self, row, column):
-        return self.board[column][row]
+    def getColor(self, x, y):
+        return self.board[y][x]
     
-    def getMarker(self, row, column):
-        return "-" if self.board[column][row] is None else self.board[column][row]
+    def getMarker(self, y, x):
+        return "-" if self.board[x][y] is None else self.board[x][y]
                     
+    def getPlayer(self):
+        return self.move_count % 2
+    
+    def four_connected_in_col(self, x):
+        for start in range(len(x) - 3):
+            colors = set(x[start:start + 4])
+            if len(colors) == 1 and not None in colors:
+                return colors.pop()
+ 
+        return None
+    
+    def get_winner(self):
+        for board_type in (self.board, self.get_transposed_board()):
+            for column in board_type:
+                winning_color = self.four_connected_in_col(column)
+                if winning_color is not None:
+                    return winning_color
+
+        return None
+   
+    def get_transposed_board(self):
+        return [[x[idx] for x in self.board] for idx in range(self.rowCount())]
+
+    def game_is_over(self):
+        return self.get_winner() is not None or self.move_count==42
+    
     def __str__(self):
         strings = ["\n"]
         strings.append((4 * " ") + "".join([("%2s" % col) for col in range(self.colCount())]))
@@ -38,7 +68,7 @@ class Board:
             strings.append(("%2s" % row) + ("%2s" % " |")\
              + "".join(["%2s" % self.getMarker(row, col) for col in range(self.colCount())]))
         return "\n".join(strings) 
-        
+    
     
     
     
--- gamebots/connect-four/test/Test_connect_four.py
+++ gamebots/connect-four/test/Test_connect_four.py
@@ -12,11 +12,11 @@ class Test(unittest.TestCase):
     def setUp(self):
         self.board=Board(6, 7)
         
-    def test_board_initialisation_rows(self):
+    def test_board_initialisation(self):
         self.assertEqual(6, self.board.rowCount())
-        
-    def test_board_initialisation_columns(self):
         self.assertEqual(7, self.board.colCount())
+        self.assertIsNone(self.board.get_winner())
+        self.assertFalse(self.board.game_is_over())  
 
     def test_move_count(self):
         self.assertTrue(self.board.isEmpty())
@@ -30,8 +30,99 @@ class Test(unittest.TestCase):
         self.board.play(2)
         self.assertEqual(1, self.board.getColor(0, 2))
         self.board.play(3)
-        print(self.board)   
         self.assertEqual(0, self.board.getColor(1, 3))
+        
+    def test_column_filled_up(self):
+        self.assertEqual(0, self.board.getPlayer())
+        self.board.play(3)
+        
+        self.assertEqual(1, self.board.getPlayer()) 
+        self.board.play(3)
+        
+        self.assertEqual(0, self.board.getPlayer())
+        self.board.play(3)
+        
+        self.assertEqual(1, self.board.getPlayer())
+        self.board.play(3)
+        
+        self.assertEqual(0, self.board.getPlayer())
+        self.board.play(3)
+        
+        self.assertEqual(1, self.board.getPlayer())
+        self.board.play(3)
+        
+        self.assertEqual(0, self.board.getPlayer())
+        self.board.play(3)
+        
+        self.assertEqual(0, self.board.getPlayer())
+        
+    def test_player_0_has_simple_winning_col(self):
+        self.board.play(1)
+        self.board.play(2)
+        self.board.play(1)
+        self.board.play(3)
+        self.board.play(1)
+        self.board.play(5)
+        self.assertIsNone(self.board.get_winner())
+        self.board.play(1)
+        self.assertEqual(0, self.board.get_winner())
+        
+    def test_player_1_has_simple_winning_col(self):
+        self.board.play(6)
+        self.board.play(1)
+        self.board.play(2)
+        self.board.play(1)
+        self.board.play(3)
+        self.board.play(1)
+        self.board.play(5)
+        self.assertIsNone(self.board.get_winner())
+        self.board.play(1)
+        self.assertEqual(1, self.board.get_winner())
+        self.assertTrue(self.board.game_is_over())
+
+    def test_player_0_has_mixed_winning_col(self):
+        self.board.play(1)
+        self.board.play(1)
+        
+        self.board.play(1)
+        self.board.play(3)
+        self.board.play(1)
+        self.board.play(5)
+        self.board.play(1)
+        self.board.play(5)
+
+        self.assertIsNone(self.board.get_winner())
+        self.board.play(1)
+        self.assertEqual(0, self.board.get_winner())
+        
+    def test_player_0_has_winning_row(self):
+        self.board.play(1)
+        self.board.play(1)
+        
+        self.board.play(2)
+        self.board.play(2)
+        self.board.play(3)
+        self.board.play(3)
+        self.assertIsNone(self.board.get_winner())
+        self.board.play(4)
+        self.assertEqual(0, self.board.get_winner())
+        
+    def test_full_board(self):
+        self.fill_board_without_winner()
+        self.assertIsNone(self.board.get_winner())
+        self.assertTrue(self.board.game_is_over())
+        print(self.board)
+    
+    def fill_board_without_winner(self):
+        for x in range(self.board.colCount()):
+            if x == 3: continue
+            for y in range(self.board.rowCount()):
+                self.board.play(x)
+        self.board.board[3][0]=1
+        for y in range(self.board.rowCount() - 1):
+            self.board.play(3)
+        self.board.move_count+=1
+    
 
 if __name__ == "__main__":
 #     suite = unittest.TestLoader().loadTestsFromTestCase(Test)
