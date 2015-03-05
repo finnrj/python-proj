a7c6bd2948ecf3857762aab12f94019dcc85042a
Finn Jensen <finnrj@gmail.com>
2015-03-05 20:55:05 +0100

    messed around with the parser

--- tiledXmlParser/XmlParser.py
+++ tiledXmlParser/XmlParser.py
@@ -10,31 +10,64 @@ class XmlParser:
     def __init__(self, tiledXmlFile):
         self.etree = xml.etree.ElementTree.parse(tiledXmlFile)
         self.root = self.etree.getroot()
-        self.objectGroups = self.etree.findall("objectgroup")
+        self.xmlObjectGroups = self.etree.findall("objectgroup")
+        self.tilesets = self.etree.findall("tileset")
+
+    def getObjectGroups(self):
+        return self.xmlObjectGroups
     
+    def getGroupByName(self, objectGroupName):
+        return ObjectGroup([og for og in self.xmlObjectGroups if og.attrib["name"] == objectGroupName][0])
     
-    def getObjectGroupName(self):
-        return self.objectGroups[0].attrib["name"]
-    
-    def getObjectGroupWidth(self):
-        return int(self.objectGroups[0].attrib["width"])
+    def getTilesets(self):
+        return self.tilesets
 
     
-    def getObjectGroupHeight(self):
-        return int(self.objectGroups[0].attrib["height"])
-
+    def getTilesetByGid(self, gid):
+        xmlTilesets = self.etree.findall("tileset")
+        return Tileset([t for t in xmlTilesets if t.attrib["firstgid"] == str(gid)][0])
     
-    def getObjects(self, objectGroupName):
-        target = [og for og in self.objectGroups if og.attrib["name"] == objectGroupName][0]
-        return [Object(e) for e in target.findall("object")]
+class Tileset:
+    def __init__(self, element):    
+        self.element = element
+        
+    def getFirstgid(self):
+        return int(self.element.attrib["firstgid"])
+    
+    def getTilewidth(self):
+        return int(self.element.attrib["tilewidth"])
+    
+    def getTileheight(self):
+        return int(self.element.attrib["tileheight"])
+    
+    def getName(self):
+        return self.element.attrib["name"]
+    
+class ObjectGroup:
+    def __init__(self, element):
+        self.element = element
+        self.objects = [Object(e) for e in element.findall("object")]
+        self.osmap = dict((o.getId(), o) for o in self.objects)
+
+    def getWidth(self):
+        return int(self.element.attrib["width"])
+
+    def getHeight(self):
+        return int(self.element.attrib["height"])
+    
+    def getObjects(self):
+        return self.objects
+    
+    def getObjectById(self, name):
+        return [o for o in self.objects if o.getId() == name][0]
 
 class Object:
     
     def __init__(self, element):
         self.element = element
     
-    def getName(self):
-        return self.element.attrib["name"]
+    def getId(self):
+        return self.element.attrib["id"]
     
     def getGid(self):
         return int(self.element.attrib["gid"])
--- tiledXmlParser/XmlParserTest.py
+++ tiledXmlParser/XmlParserTest.py
@@ -6,6 +6,7 @@ Created on Feb 5, 2015
 import unittest
 from XmlParser import XmlParser
 from io import StringIO
+import os
 
 
 class Test(unittest.TestCase):
@@ -14,43 +15,65 @@ class Test(unittest.TestCase):
     def testSingleObjectObjectGroup(self):
         xml = StringIO("""<map version="1.0" orientation="orthogonal" width="40" height="15" tilewidth="32" tileheight="32">
                                 <objectgroup name="Objects" width="0" height="0">
-                                      <object name="player" gid="14" x="27" y="416"/>
+                                      <object id="player" gid="14" x="27" y="416"/>
                                 </objectgroup>
                           </map>""")
         
         self.parser = XmlParser(xml)
-        self.assertEqual("Objects", self.parser.getObjectGroupName())
-        self.assertEqual(0, self.parser.getObjectGroupWidth())
-        self.assertEqual(0, self.parser.getObjectGroupHeight())
-        self.assertIsNotNone(self.parser.getObjects("Objects"))
-        self.assertEqual(1, len(self.parser.getObjects("Objects")))
-        self.assertEqual("player", self.parser.getObjects("Objects")[0].getName())
-        self.assertEqual(14, self.parser.getObjects("Objects")[0].getGid())
-        self.assertEqual(27, self.parser.getObjects("Objects")[0].getX())
-        self.assertEqual(416, self.parser.getObjects("Objects")[0].getY())
+        objectGroup = self.parser.getGroupByName("Objects")
+        self.assertEqual(0, objectGroup.getWidth())
+        self.assertEqual(0, objectGroup.getHeight())
+        
+        objects = objectGroup.getObjects()
+        self.assertIsNotNone(objects)
+        self.assertEqual(1, len(objects))
+        self.assertEqual("player", objects[0].getId())
+        self.assertEqual(14, objects[0].getGid())
+        self.assertEqual(27, objects[0].getX())
+        self.assertEqual(416, objects[0].getY())
                 
     def testMultipleObjectsObjectGroup(self):
         xml = StringIO("""<map version="1.0" orientation="orthogonal" width="40" height="15" tilewidth="32" tileheight="32">
                                 <objectgroup name="Objects2" width="1" height="2">
-                                      <object name="player" gid="14" x="27" y="416"/>
-                                      <object name="hubba" gid="25" x="50" y="100"/>                              
+                                      <object id="player" gid="14" x="27" y="416"/>
+                                      <object id="hubba" gid="25" x="50" y="100"/>                              
                                 </objectgroup>
                           </map>""")
         self.parser = XmlParser(xml)
-        self.assertEqual("Objects2", self.parser.getObjectGroupName())
-        self.assertEqual(1, self.parser.getObjectGroupWidth())
-        self.assertEqual(2, self.parser.getObjectGroupHeight())
-        self.assertIsNotNone(self.parser.getObjects("Objects2"))
-        self.assertEqual(2, len(self.parser.getObjects("Objects2")))
-        self.assertEqual("hubba", self.parser.getObjects("Objects2")[1].getName())
-        self.assertEqual(25, self.parser.getObjects("Objects2")[1].getGid())
-        self.assertEqual(50, self.parser.getObjects("Objects2")[1].getX())
-        self.assertEqual(100, self.parser.getObjects("Objects2")[1].getY())
-    
-
+        objectsGroups2 = self.parser.getGroupByName("Objects2")
+        self.assertIsNotNone(objectsGroups2)
+        self.assertEqual(1, objectsGroups2.getWidth())
+        self.assertEqual(2, objectsGroups2.getHeight())
+        objects = objectsGroups2.getObjects()
+        self.assertIsNotNone(objects)
+        self.assertEqual(2, len(objects))
         
+        self.assertEqual("hubba", objects[1].getId())
+        object1 = objectsGroups2.getObjectById("hubba")
+        self.assertEqual(25, object1.getGid())
+        self.assertEqual(50, object1.getX())
+        self.assertEqual(100, object1.getY())
+        
+    def testSmallMap(self):
+        self.parser = XmlParser(open("small_map/small_map.tmx"))
+        self.assertEqual(3, len(self.parser.getObjectGroups()))
+         
+    def testSmallMapGroupName(self):
+        self.parser = XmlParser(open("small_map/small_map.tmx"))
+        wallsGroup = self.parser.getGroupByName("walls")
+        self.assertIsNotNone(wallsGroup)
+        
+    def testTileset(self):
+        self.parser = XmlParser(open("small_map/small_map.tmx"))
+        self.assertEqual(3, len(self.parser.getTilesets()))
+        evilStickman = self.parser.getTilesetByGid(3)
+        self.assertEqual(3, evilStickman.getFirstgid())
+        self.assertEqual(32, evilStickman.getTilewidth())
+        self.assertEqual(64, evilStickman.getTileheight())
+
 
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
+    
