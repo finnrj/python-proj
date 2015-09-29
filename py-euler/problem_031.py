4357a0c4d9eb2870cb8bd3d752ee7e34b196f417
Torsten Knauf <Torsten.Knauf@gmx.de>
2015-09-30 00:16:33 +0200

    Solved coin problem
    You were right that I can't figure it out with paper and pencil easily
    -> any wishes for cakes or similar? :P

--- py-euler/problem_031.py
+++ py-euler/problem_031.py
@@ -10,8 +10,49 @@ It is possible to make £2 in the following way:
 
 How many different ways can £2 be made using any number of coins?
 
+Others guy solution which looks so smart but I don't understand -.-
+public class problem31 {
+    final static int TOTAL = 200;
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5, 10, 20, 50, 100, 200};
+        int[] ways = new int[TOTAL + 1];
+        ways[0] = 1;
+
+        for(int coin: coins)
+            for(int j = coin; j <= TOTAL; j++)
+                ways[j] += ways[j - coin];
+
+        System.out.println("Result: " + ways[TOTAL]);
+    }
+}
+
 '''
 
+coinsPrime = [1, 2, 5, 10, 20, 50, 100, 200]
+coins = coinsPrime[::-1]
+
+targetSum = 200
+
+def helper(i, s):
+    if i >= len(coins):
+        return 0
+    
+    if s + coins[i] == 200:
+        return 1 + helper(i + 1, s)
+    
+    if s + coins[i] > 200:
+        return helper(i + 1, s)
+    
+    # s < targetSum
+    #                    take coin    take not
+    return helper(i, s + coins[i]) + helper(i + 1, s)
 
 if __name__ == '__main__':
-    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    print(helper(0, 0))
+    
+    
+        
+        
+        
+        
