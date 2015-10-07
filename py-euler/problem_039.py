342ec94f1df2d1c506cdef4122fc730bb2e6b946
Finn Jensen <finnrj@gmail.com>
2015-10-07 21:36:29 +0200

    played around a bit and remembered the good old 'timeit' :-)

--- py-euler/problem_039.py
+++ py-euler/problem_039.py
@@ -6,6 +6,7 @@ here are exactly three solutions for p = 120.
 
 For which value of p ≤ 1000, is the number of solutions maximised?
 '''
+import timeit
 
 def isRightAngleTriangle(a, b, p):
     c = p - a - b
@@ -14,6 +15,13 @@ def isRightAngleTriangle(a, b, p):
 def solutions(p):
     return len([1 for a in range(p // 3) for b in range(a, p // 2) if isRightAngleTriangle(a, b, p)])
 
+def solutions2(p):
+    return len([1 for a in range(p // 3) for b in range(a, p // 2) if a * b % 12 == 0 and isRightAngleTriangle(a, b, p)])
 
 if __name__ == '__main__':
-    print(max([(solutions(p), p) for p in range(3, 1001)]))
+    firstSolution = 'print(max([(solutions(p), p) for p in range(3, 1001)]))'
+    print(timeit.timeit(firstSolution, setup='from __main__ import solutions', number=3))
+
+    secondSolution = 'print(max([(solutions2(p), p) for p in range(3, 1001)]))'
+    print(timeit.timeit(secondSolution, setup='from __main__ import solutions2', number=3))
+    
