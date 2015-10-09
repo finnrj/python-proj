90a9bce85614e3487876cc1466a65375969e1ff5
Finn Jensen <finnrj@gmail.com>
2015-10-10 00:14:58 +0200

    challenge accepted :-)

--- py-euler/problem_039.py
+++ py-euler/problem_039.py
@@ -21,13 +21,20 @@ def solutions2(p):
 def solutions3(p):
     return len([1 for a in range(p // 3) for b in range(a, (p - a) // 2) if a * b % 12 == 0 and isRightAngleTriangle(a, b, p)])
 
+def solutions4(p):
+    return len([1 for a in range(p // 3) for b in range(a, (p - a) // 2) if a * b % 12 == 0 and a * b * (p - a - b) % 60 == 0 and isRightAngleTriangle(a, b, p)])
+
 if __name__ == '__main__':
+    rounds = 3
     firstSolution = 'print(max([(solutions(p), p) for p in range(3, 1001)]))'
-    print(timeit.timeit(firstSolution, setup='from __main__ import solutions', number=3))
- 
+    print(timeit.timeit(firstSolution, setup='from __main__ import solutions', number=rounds) / rounds)
+  
     secondSolution = 'print(max([(solutions2(p), p) for p in range(3, 1001)]))'
-    print(timeit.timeit(secondSolution, setup='from __main__ import solutions2', number=3))
-    
+    print(timeit.timeit(secondSolution, setup='from __main__ import solutions2', number=rounds) / rounds)
+
     thirdSolution = 'print(max([(solutions3(p), p) for p in range(3, 1001)]))'
-    print(timeit.timeit(thirdSolution, setup='from __main__ import solutions3', number=3))
+    print(timeit.timeit(thirdSolution, setup='from __main__ import solutions3', number=rounds) / rounds)
+
+    fourthSolution = 'print(max([(solutions4(p), p) for p in range(3, 1001)]))'
+    print(timeit.timeit(fourthSolution, setup='from __main__ import solutions4', number=rounds) / rounds)
     
