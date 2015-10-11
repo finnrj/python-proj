9beea1944a95258240f6a8c7763c152e823ddd4f
Finn Jensen <finnrj@gmail.com>
2015-10-11 09:12:23 +0200

    corrected path in 007, final(?) optimization for 039 :-)

--- py-euler/problem_007.py
+++ py-euler/problem_007.py
@@ -5,5 +5,5 @@ Created on Apr 21, 2015
 '''
 
 if __name__ == '__main__':
-    lines = open("primes1.txt").readlines()[2:]
+    lines = open("utilities/primes.txt").readlines()[2:]
     print([int(p) for l in lines for p in l.split()][10000])
--- py-euler/problem_039.py
+++ py-euler/problem_039.py
@@ -24,17 +24,23 @@ def solutions3(p):
 def solutions4(p):
     return len([1 for a in range(p // 3) for b in range(a, (p - a) // 2) if a * b % 12 == 0 and a * b * (p - a - b) % 60 == 0 and isRightAngleTriangle(a, b, p)])
 
+def solutions5(p):
+    return len([1 for a in range(p // 3) for b in range(a, (p - a) // 2) if a * b % 12 == 0 and a * b * (p - a - b) % 60 == 0 and isRightAngleTriangle(a, b, p)])
+
 if __name__ == '__main__':
     rounds = 3
     firstSolution = 'print(max([(solutions(p), p) for p in range(3, 1001)]))'
     print(timeit.timeit(firstSolution, setup='from __main__ import solutions', number=rounds) / rounds)
-  
+   
     secondSolution = 'print(max([(solutions2(p), p) for p in range(3, 1001)]))'
     print(timeit.timeit(secondSolution, setup='from __main__ import solutions2', number=rounds) / rounds)
-
+ 
     thirdSolution = 'print(max([(solutions3(p), p) for p in range(3, 1001)]))'
     print(timeit.timeit(thirdSolution, setup='from __main__ import solutions3', number=rounds) / rounds)
 
     fourthSolution = 'print(max([(solutions4(p), p) for p in range(3, 1001)]))'
     print(timeit.timeit(fourthSolution, setup='from __main__ import solutions4', number=rounds) / rounds)
+
+    fifthSolution = 'print(max([(solutions5(p), p) for p in range(12, 1002, 2)]))'
+    print(timeit.timeit(fifthSolution, setup='from __main__ import solutions5', number=rounds) / rounds)
     
