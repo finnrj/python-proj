ad0741721ff62cec9a025e8c34908d0893ecb472
Torsten Knauf <Torsten.Knauf@gmx.de>
2015-10-21 17:01:40 +0200

    solved problem 40 and added problem 41-43 through the script, just 'cause I can :) (now we only need to add about 3.163.936 more problems until the script saved me some time :P )

---
+++ py-euler/problem_040.py
@@ -0,0 +1,22 @@
+'''
+
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.123456789101112131415161718192021...
+
+It can be seen that the 12th digit of the fractional part is 1.
+
+If dn represents the nth digit of the fractional part, find the value of the following expression.
+
+d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+
+'''
+
+from math import log10
+from _functools import reduce
+from _operator import mul
+
+if __name__ == '__main__':
+    n = "".join([str(i) for i in range(1000000)])
+    targets = [int(n[10 ** i]) for i in range(int(log10(1000000)) + 1)]       
+    print(reduce(mul, targets))
