a359da785f6b7a0842a6a7698a49a91b062a103a
finn <finnrj@gmail.com>
2016-04-05 21:48:11 +0200

    life is soooo hard...

--- py-euler/problem_054.py
+++ py-euler/problem_054.py
@@ -60,46 +60,147 @@ class Hand:
 	ranks = dict(zip(string.digits[2:] + "TJQKA", range(2, 15))) 
 	
 	def __init__(self, cards):
-		self.values = sorted([self.ranks[card[0]] for card in cards], reverse=True)
+		'''	cards = ([2H, 3D, KH, TS, 4C] ''' 
+# ToDo better algorithm		
+		values = [self.ranks[card[0]] for card in cards]
+		self.values = sorted(values, reverse=True)
 		self.colors = [card[1] for card in cards]	
 	
 	def __str__(self):
-		return "%s, %s" % (self.values, self.colors)
+# 		return "%s, %s" % (self.values, self.colors)
+		return "%s" % (self.values)
 
 def hasPair(hand):
 	return len(set(hand.values)) == 4
 
-if __name__ == '__main__':
-	with open("utilities/poker.txt") as fil:
-		hands = [(Hand(line.strip().split()[:5]), Hand(line.strip().split()[5:]))  for line in fil.readlines()]
-		print ([str(h) for h in hands[0]])
+def hasTwoPairs(hand):
+	return len(set(hand.values)) <= 3
 
+def hasThreeOfAKind(hand):
+	return hand.values.count(hand.values[0]) >= 3 or hand.values.count(hand.values[1]) >= 3 or hand.values.count(hand.values[2]) >= 3 
 
 def hasStraight(hand):
 	return hand.values[0] - hand.values[-1] == 4 and len(set(hand.values)) == 5 	
 
-def hasRoyalFlush(hand):
-	return len(set(hand.colors)) == 1 and hasStraight(hand)
+def hasFlush(hand):
+	return len(set(hand.colors)) == 1
 
 def hasFullHouse(hand):
 	someValue = hand.values.count(hand.values[0])
 	return len(set(hand.values)) == 2 and someValue >= 2 and someValue <= 3
 
-class TestCase(unittest.TestCase):
+def hasFourOfAKind(hand):
+	return hand.values.count(hand.values[0]) == 4 or hand.values.count(hand.values[1]) == 4
 
+def hasStraightFlush(hand):
+	return hasFlush(hand) and hasStraight(hand)
+
+def hasRoyalFlush(hand):
+	return hasFlush(hand) and hasStraight(hand) and hand.values[0] == 14
+
+rankMethods = [
+			hasRoyalFlush,
+			hasStraightFlush,
+			hasFourOfAKind,
+			hasFullHouse,
+			hasFlush,
+			hasStraight,
+			hasThreeOfAKind,
+			hasTwoPairs,
+			hasPair]
+
+def valuesToInt(hand):
+	if hasFullHouse(hand):
+		return int(hand.values[2])
+	
+	if hasTwoPairs(hand):
+		return
+	if hasPair(hand):
+		return
+	
+# 	spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaass
+	
+	return int("".join(["%02d" % val for val in hand.values]))	
+	
+def getWinner(hands):
+	hand0, hand1 = hands
+	
+	for rankMethod in rankMethods:
+		if rankMethod(hand0) and not rankMethod(hand1):
+			return 0
+		if rankMethod(hand1) and not rankMethod(hand0):
+			return 1
+# 	print(hand0, hand1, 0 if valuesToInt(hand0) > valuesToInt(hand1) else 1)
+	return 0 if valuesToInt(hand0) > valuesToInt(hand1) else 1
+
+if __name__ == '__main__':
+	with open("utilities/poker.txt") as fil:
+		deals = [(Hand(line.strip().split()[:5]), Hand(line.strip().split()[5:])) for line in fil.readlines() if line.strip()]
+# 		for d1, d2 in deals:
+# 			print(d1, d2)
+		print(len(deals))
+		print([getWinner(deal) for deal in deals].count(0))
+
+class TestCase(unittest.TestCase):
+	royalFlush = Hand(['AC', 'KC', 'QC', 'TC', 'JC'])
+	straightFlush = Hand(['AC', 'KC', 'QC', 'TC', 'JC'])
+	flush = Hand(['AC', 'KC', '2C', 'TC', 'JC'])
+	fourOfAKind = Hand(['8C', '8D', '8S', '8H', '4S'])
+	fullHouse = Hand(['AC', 'AH', 'AS', '8C', 'JC'])
+	threeOfAKind = Hand(['8C', '8D', 'KC', '8H', '4S'])
+	twoPairs = Hand(['8C', 'TS', 'TC', '8H', '4S'])
+	pair = Hand(['8C', 'TS', 'KC', '8H', '4S'])
+		
 	def testHasPair(self):
-		self.assertTrue(hasPair(Hand(['8C', 'TS', 'KC', '8H', '4S'])))
+		self.assertTrue(hasPair(self.pair))
 		self.assertFalse(hasPair(Hand(['9C', 'TS', 'KC', '8H', '4S'])))	
 
-	def testHasRoyalFlush(self):
-		self.assertTrue(hasRoyalFlush(Hand(['AC', 'KC', 'QC', 'TC', 'JC'])))
-		self.assertFalse(hasRoyalFlush(Hand(['AC', 'KC', 'QC', '9C', 'JC'])))	
+	def testHasTwoPairs(self):
+		self.assertTrue(hasTwoPairs(self.twoPairs))
+		self.assertFalse(hasTwoPairs(Hand(['8C', '4D', 'KC', '7H', '4S'])))
+		self.assertTrue(hasTwoPairs(Hand(['8C', 'TS', 'TC', '8H', '8S'])))
+		self.assertTrue(hasTwoPairs(Hand(['8C', 'TS', '8D', '8H', '8S'])))
 
+	def testHasThreeOfAKind(self):
+		self.assertTrue(hasThreeOfAKind(self.threeOfAKind))
+		self.assertFalse(hasThreeOfAKind(Hand(['8C', 'TS', 'TC', '8H', '4S'])))
+		self.assertTrue (hasThreeOfAKind(Hand(['8C', 'TS', 'TC', '8H', '8S'])))
+		self.assertTrue (hasThreeOfAKind(Hand(['8C', 'TS', '8D', '8H', '8S'])))
+
+	def testHasFlush(self):
+		self.assertTrue(hasFlush(self.flush))
+		self.assertFalse(hasFlush(Hand(['AC', 'KD', 'QC', '9C', 'JC'])))			
+		
 	def testHasFullHouse(self):
-		self.assertFalse(hasFullHouse(Hand(['AC', 'AH', 'AS', '8C', 'JC'])))
+		self.assertFalse(hasFullHouse(self.fullHouse))
 		self.assertFalse(hasFullHouse(Hand(['AC', 'AH', 'AS', 'AD', 'JC'])))
 		self.assertTrue(hasFullHouse(Hand(['AC', 'AH', 'AS', '8C', '8H'])))
 		
+	def testHasFourOfAKind(self):
+		self.assertTrue (hasFourOfAKind(self.fourOfAKind))
+		self.assertFalse(hasFourOfAKind(Hand(['8C', 'TS', 'TC', '8H', '4S'])))
+		self.assertFalse(hasFourOfAKind(Hand(['8C', 'TS', 'TD', '8H', 'TS'])))
+
+	def testHasStraightFlush(self):
+		self.assertTrue(hasStraightFlush(self.straightFlush))
+		self.assertTrue(hasStraightFlush(Hand(['TC', 'KC', 'QC', '9C', 'JC'])))	
+		self.assertFalse(hasStraightFlush(Hand(['TC', 'KC', '8C', '9C', 'JC'])))
+		self.assertFalse(hasStraightFlush(Hand(['TC', 'KC', 'QD', '9C', 'JC'])))
+				
+	def testHasRoyalFlush(self):
+		self.assertTrue(hasRoyalFlush(self.royalFlush))
+		self.assertFalse(hasRoyalFlush(Hand(['9C', 'KC', 'QC', 'TC', 'JC'])))
+		self.assertFalse(hasRoyalFlush(Hand(['AC', 'KC', 'QC', '9C', 'JC'])))	
+		
+	def testGetWinner(self):
+		self.assertEqual(1, getWinner((self.straightFlush, self.royalFlush)))
+		self.assertEqual(0, getWinner((self.straightFlush, self.fourOfAKind)))
+		self.assertEqual(0, getWinner((self.fourOfAKind, self.fullHouse)))
+		self.assertEqual(0, getWinner((self.fullHouse, self.pair)))
+		self.assertEqual(1, getWinner((Hand(['9C', 'KD', 'QC', 'TS', '2C']), self.pair)))
+		self.assertEqual(1, getWinner((Hand(['9C', '4D', '6C', '7S', '2C']), Hand(['9D', '3D', '5C', '8S', '2C']))))
+		self.assertEqual(0, getWinner((Hand(['9C', '9H', '9D', '2S', '2C']), Hand(['8D', '8C', '8H', 'AS', 'AC']))))
+		self.assertEqual(1, getWinner((Hand(['QC', 'TH', 'TD', '8S', '5C']), Hand(['JD', 'JC', '8H', '6S', '2C']))))
 	if __name__ == '__main__':
 		unittest.main()
 	
