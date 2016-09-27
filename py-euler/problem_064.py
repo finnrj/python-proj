191ec071fd8b9d91a0a987b474c2cad5530d571d
finn <finnrj@gmail.com>
2016-09-27 21:14:31 +0200

    could have worked.... mooooep

--- py-euler/problem_064.py
+++ py-euler/problem_064.py
@@ -10,6 +10,34 @@ written in the form:
 1 
 
 '''
+from math import floor, sqrt
+ 
+def testPeriod(x, bucket, delta=0.000001):
+	return True in [(abs(decs - x)) < delta for decs in bucket]
+
+def testPeriod2(x, bucket, delta=0.000001):
+	for decs in bucket:
+		if((abs(decs - x)) < delta): 
+			return True
+	return False 
+
+
+def doIt(root):
+	n = sqrt(root)
+	bucket = []
+	while True:
+		i = floor(n)
+		decs = n - i
+		if  testPeriod2(decs, bucket):
+			print("root : %03d,  period: %d" % (root, len(bucket)))
+			return len(bucket)
+		bucket.append(decs)
+		n = 1 / decs
+	return bucket
 
 if __name__ == '__main__':
-	pass
+	periods = [doIt(root) for root in range (2, 10001) if floor(sqrt(root)) != sqrt(root)]
+	print(sum([(p % 2) for p in periods]))
+	print(max(periods))
+
+
