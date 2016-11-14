4cd19f8823bd46103c765521d66c64b3c5d3c270
Torsten89 <Torsten.Knauf@gmx.de>
2016-11-14 23:00:53 +0100

    magic

--- py-euler/utilities/RegularContinuedFraction.py
+++ py-euler/utilities/RegularContinuedFraction.py
@@ -23,6 +23,7 @@ or
 
 [a_0; a_1, a_2, ..., a_k]
 '''
+from _pydecimal import _sqrt_nearest
 from fractions import Fraction
 from functools import reduce
 from math import floor, sqrt
@@ -60,24 +61,30 @@ def regularContinuedFractionToFraction(regContFrac):
         lambda acc, a_i: Fraction(acc.denominator, a_i * acc.denominator + acc.numerator),
         reversed(regContFrac[1:-1]),
         Fraction(1, regContFrac[-1]))
+
+
+def getRepeatingRegContFrac(sqrtArg):
+    """ return list which represents a periodic continued fraction, where list[1:] is periodic
+        m_0 = 0, d_0 = 1,  a_0 = _sqrt_nearest(sqrtArg), 
+        m_(n+1) = d_n * a_n - m_n
+        d_(n+1) = floor((sqrtArg - m_(n+1)^2) / d_n 
+        a_(n+1) = (sqrtArg + m_(n+1)) / d_(n+1)
+    """
+    magic = []  # m_n, d_n, a_n
+    a_0 = floor(sqrt(sqrtArg))
+    nextTriple = (0, 1, a_0)
     
-def getRepeatingRegContFrac(sqrtX):
-    ''' Every √x can be written through a repeating continued fraction. For example:
-        √3 = 1 + √3 - 1
-           = 1 + (√3 - 1) * 1
-           = 1 + (√3 - 1) * (√3 + 1) / (√3 + 1) 
-           = 1 + (3 - 1) / (√3 + 1) 
-           = 1 + 2 / (√3 + 1) expand with 1/2 so that numerator becomes 1
-           = 1 + 1 / (1/2 * (√3 + 1))
-        The previous line is equal to √3. Therefore we can replace √3 with the whole line
-           = 1 + 1 / (1/2 * ([1 + 1 / (1/2 * (√3 + 1))] + 1))
-           = 1 + 1 / (1/2 * ([2 + 1 / (1/2 * (√3 + 1))]))
-           = 1 + 1 / (1 + 1 / (√3 + 1)) numerator is already 1
-        Replace √3 again in this term with the previous mentioned line
-        ...
-        Repeat until the continued Fraction repeats itself
+    
+    while nextTriple not in magic:
+        magic.append(nextTriple)
         
-        return [1, 1, 2] meaning the next number would have been already in there
-    '''
+        old_m, old_d, old_a = magic[-1]
+        m = old_d * old_a - old_m
+        d = (sqrtArg - m ** 2) / old_d
+        a = floor((a_0 + m) / d)
+        nextTriple = (m, d, a)
     
-    pass
+    return [a for (m, d, a) in magic]
+
+print(getRepeatingRegContFrac(2))    
+print(getRepeatingRegContFrac(14))
