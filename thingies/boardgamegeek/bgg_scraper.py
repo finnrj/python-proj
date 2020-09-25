c6aa4d62a60fd134345eaf7e5e03c4ddcdffc0b5
finn <finnrj@gmail.com>
2020-09-25 18:29:39 +0200

    getting closer with indices

--- thingies/boardgamegeek/bgg_scraper.py
+++ thingies/boardgamegeek/bgg_scraper.py
@@ -13,8 +13,9 @@ def main_soup():
     target_url = "https://boardgamegeek.com/browse/boardgame"
     html_text = requests.get(target_url).text
     soup = BeautifulSoup(html_text, 'html.parser')
-    rs = [child  for row in soup.find_all(id='row_')[:] for child in row.children]
+    rs = [child for row in soup.find_all(id='row_')[:] for child in row.children]
     print(rs)
+
 
 def main():
     target_url = "https://boardgamegeek.com/browse/boardgame"
@@ -23,20 +24,6 @@ def main():
     #         shutil.copyfileobj(resp, fil)
     #         filename = fil.name
 
-    # with open(filename) as html:
-    #     lines = html.readlines()
-    #     print(len(lines))
-    # <a href="/boardgame/161936/pandemic-legacy-season-1" class="primary">Pandemic Legacy: Season 1</a>
-    # Pandemic Legacy: Season 1
-    # /html/body/div[2]/main/div/div[1]/div[1]/div/div/div[2]/div[3]/table/tbody/tr[3]/td[3]/div[2]/a
-
-    # <div id="results_objectname2" style="z-index:1000;" onclick="">
-    # <a href="/boardgame/161936/pandemic-legacy-season-1" class="primary">Pandemic Legacy: Season 1</a>
-    # <span class="smallerfont dull">(2015)</span>
-    # </div>
-    # name_regex = re.compile('<div id="results_objectname.*</div>"')
-    # print(name_regex.search(target_lines))
-    # name_lines = name_regex.findall(target_lines)
     target_lines = open("target.html").readlines()[274:]
     target_lines = [line.strip().replace('\t', '') for line in target_lines if len(line.strip()) > 0]
     rows = []
@@ -51,17 +38,49 @@ def main():
             append = False
             rows.append(row)
 
-    name_lines = [line for line in target_lines if line.strip().startswith('<a  href="/boardgame/')]
+    fetch_names(rows)
+    fetch_ranks(rows)
+    fetch_description(rows)
+    fetch_rating(rows)
+    fetch_votes(rows)
+
+    # for idx, r in enumerate(rows[1]):
+    #     print("%2d:%s" % (idx, r))
+    # print(rows[1])
+
+
+def fetch_rating(rows):
+    rating_regex = re.compile('(\d+.\d+)')
+    rating = [rating_regex.findall(r[21])[0] for r in rows]
+    print(rating)
+
+
+def fetch_votes(rows):
+    vote_regex = re.compile('(\d+)')
+    votes = [vote_regex.findall(r[25])[0] for r in rows]
+    print(votes)
+
+
+def fetch_description(rows):
+    description = [r[17] for r in rows]
+    print(description)
+
+
+def fetch_ranks(rows):
+    rank_regex = re.compile(r'.*name="(\d+)">')
+    ranks = [rank_regex.findall(r[2])[0] for r in rows]
+    print(ranks)
+
+
+def fetch_names(rows):
+    name_lines = [line[13] for line in rows]
     print(name_lines[0])
     name_regex = re.compile(r'href="(.*)".*>(.*)</a>')
     tuples = [name_regex.findall(line) for line in name_lines]
     dd = reduce(lambda l1, l2: l1 + l2, tuples)
     d = dict(dd)
     print(d)
-    print(rows[0])
-    for idx, r in enumerate(rows[1]):
-        print("%2d:%s" % (idx,r))
-    # print(rows[1])
+
 
 if __name__ == '__main__':
     main()
