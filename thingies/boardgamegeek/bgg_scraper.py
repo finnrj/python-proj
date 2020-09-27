ebeedf08021823700831ba137f05667382374b77
finn <finnrj@gmail.com>
2020-09-27 14:25:44 +0200

    going a bit OO

--- thingies/boardgamegeek/bgg_scraper.py
+++ thingies/boardgamegeek/bgg_scraper.py
@@ -1,13 +1,52 @@
-import shutil
-import tempfile
+import re
+from functools import reduce
 
 import requests
 from bs4 import BeautifulSoup
-from urllib import request, response
-from functools import reduce
 
-import re
 
+class BGGRow:
+    def __init__(self, rank, name, description, image_link, rating, votes):
+        self.rank = int(rank)
+        self.rank_marker = ""
+        self.name = name
+        self.description = description
+        self.image_link = image_link
+        self.rating = float(rating)
+        self.rating_marker = ""
+        self.votes = int(votes)
+
+    def update(self, other):
+        self.set_rank_marker(other.rank)
+        self.rank = other.rank
+        self.set_rating_marker(other.rating)
+        self.rating = other.rating
+        if not self.description is other.description:
+            self.description = other.description
+        if not self.image_link is other.image_link:
+            self.image_link = other.image_link
+        self.votes = other.votes
+
+    def set_rank_marker(self, new_rank):
+        if new_rank > self.rank:
+            self.rank_marker = '(^^)'
+        elif new_rank < self.rank:
+            self.rank_marker = '(vv)'
+        else:
+            self.rank_marker = ""
+
+    def set_rating_marker(self, new_rating):
+        if new_rating > self.rank:
+            self.rating_marker = '(++)'
+        elif new_rating < self.rank:
+            self.rating_marker = '(--)'
+        else:
+            self.rating_marker = ""
+
+    def __str__(self):
+        template = "%3d %4s,%-30s, %2.3f, %7d"
+        return template % (self.rank, self.rank_marker,
+                           self.name, self.rating, self.votes)
 
 def main_soup():
     target_url = "https://boardgamegeek.com/browse/boardgame"
@@ -38,48 +77,48 @@ def main():
             append = False
             rows.append(row)
 
-    fetch_names(rows)
-    fetch_ranks(rows)
-    fetch_description(rows)
-    fetch_rating(rows)
-    fetch_votes(rows)
-
-    # for idx, r in enumerate(rows[1]):
-    #     print("%2d:%s" % (idx, r))
-    # print(rows[1])
+    keys, names = fetch_names(rows)
+    elements = list(zip(fetch_ranks(rows), names, fetch_description(rows), fetch_image_links(rows),
+                        fetch_rating(rows), fetch_votes(rows)))
+    elements = [BGGRow(rank, name, description, image_link, rating, votes) for rank, name, description, image_link, rating, votes in elements]
+    data = dict(zip(keys, elements))
+    print(data)
+    for k,e in data.items():
+        if e.rank <= 3:
+            print(k)
+            print(e)
 
 
 def fetch_rating(rows):
     rating_regex = re.compile('(\d+.\d+)')
-    rating = [rating_regex.findall(r[21])[0] for r in rows]
-    print(rating)
+    return [rating_regex.findall(r[21])[0] for r in rows]
 
 
 def fetch_votes(rows):
     vote_regex = re.compile('(\d+)')
-    votes = [vote_regex.findall(r[25])[0] for r in rows]
-    print(votes)
+    return [vote_regex.findall(r[25])[0] for r in rows]
 
 
 def fetch_description(rows):
-    description = [r[17] for r in rows]
-    print(description)
+    return [r[17] for r in rows]
 
 
 def fetch_ranks(rows):
     rank_regex = re.compile(r'.*name="(\d+)">')
-    ranks = [rank_regex.findall(r[2])[0] for r in rows]
-    print(ranks)
+    return [rank_regex.findall(r[2])[0] for r in rows]
+
+
+def fetch_image_links(rows):
+    image_regex = re.compile(r'.*src="(.*)"></a>')
+    return [image_regex.findall(r[5])[0] for r in rows]
 
 
 def fetch_names(rows):
     name_lines = [line[13] for line in rows]
-    print(name_lines[0])
     name_regex = re.compile(r'href="(.*)".*>(.*)</a>')
     tuples = [name_regex.findall(line) for line in name_lines]
     dd = reduce(lambda l1, l2: l1 + l2, tuples)
-    d = dict(dd)
-    print(d)
+    return zip(*dd)
 
 
 if __name__ == '__main__':
